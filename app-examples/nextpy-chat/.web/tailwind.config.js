8dda17070217c83025dec17bea3d3159eb6b19b2
Raj <84141638+Raj4646@users.noreply.github.com>
2024-01-03 21:41:01 +0530

    Delete app-examples/nextpy-chat/.web directory

--- app-examples/nextpy-chat/.web/tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-	content: ["./pages/**/*.{js,ts,jsx,tsx}"],
-	theme: null,
-	plugins: [
-		require("@tailwindcss/typography"),
-	],
-};
